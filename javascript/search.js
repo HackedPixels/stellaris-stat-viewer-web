172bd027299d56ef4013f7fdfdfb7c0df96b3909
Jan <jan@koeppen.name>
2019-05-30 02:33:04 +0200

    added placeholder icon, added warning

--- javascript/search.js
+++ javascript/search.js
@@ -5,6 +5,7 @@ function searchTable() {
 
 	var tr = table.getElementsByTagName("tr");
 	var td, txtValue;
+	var visible = 0;
 
 	for (var i = 0; i < tr.length; i++) {
 		td = tr[i].getElementsByTagName("td")[0];
@@ -12,9 +13,17 @@ function searchTable() {
 			txtValue = td.textContent || td.innerText;
 			if (txtValue.toUpperCase().indexOf(filter) > -1) {
 				tr[i].style.display = "";
+				visible++;
 			} else {
 				tr[i].style.display = "none";
 			}
 		}
+	}
+
+	var chickens = document.getElementById("chicken")
+	if (visible === 0) {
+		chickens.style.display = "";
+	} else {
+		chickens.style.display = "none";
 	}
 }

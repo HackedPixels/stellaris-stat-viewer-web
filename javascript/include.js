56d41682e338f8d6b51e0921edd82c86dbb64bca
Jan <jan@koeppen.name>
2019-06-05 18:24:02 +0200

    working on login

--- javascript/include.js
+++ javascript/include.js
@@ -1,6 +1,11 @@
 function includeFiles() {
 	var x = document.getElementsByClassName("include-file");
 	for (var i = 0; i < x.length; i++) {
-		x[i].innerHTML = "gello";
+		// TODO: add file loading.
+		readTextFile(x[i].dataset.file, includeFile, x[i].id)
 	}
+}
+
+function includeFile(id, content) {
+	document.getElementById(id).innerHTML = content;
 }
